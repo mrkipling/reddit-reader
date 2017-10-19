f956116215f742426db0fe46e136fc0d97611965
Brad Abrahams <email@bradabrahams.co.uk>
2017-10-19 17:16:23 +0100

    Log redux actions

--- assets/js/store/configureStore.js
+++ assets/js/store/configureStore.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import reducers from '../reducers/reducers';
 
 export default function configureStore(initialState) {
-  return createStore(reducers, initialState, applyMiddleware(thunk));
+  return createStore(reducers, initialState, applyMiddleware(thunk, logger));
 }
