ed62fded1bb640272a2c237f0095a6723ecbc29b
Brad Abrahams <email@bradabrahams.co.uk>
2017-10-19 15:20:59 +0100

    Actually link submissions

--- assets/js/components/FullPost.jsx
+++ assets/js/components/FullPost.jsx
@@ -6,10 +6,18 @@ import PostDetails from './PostDetails';
 
 class FullPost extends React.Component {
   render() {
+    console.log(this.props.post);
+
+    const title = this.props.post.is_self ? (
+      <span>{this.props.post.title}</span>
+    ) : (
+      <a href={this.props.post.url} target="_blank" rel="noopener noreferrer">{this.props.post.title}</a>
+    );
+
     return !this.props.post.comments ? null : (
       <div className="full-post">
         <header className="full-post__header card">
-          <h2 className="full-post__header__title">{this.props.post.title}</h2>
+          <h2 className="full-post__header__title">{title}</h2>
           <PostDetails post={this.props.post} />
           <div className="full-post__header__text usertext" dangerouslySetInnerHTML={{ __html: this.props.post.selftext_html }} />
         </header>
