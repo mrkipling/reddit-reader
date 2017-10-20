7eb42a5eb5342df6bbec9d4540402c9d58824cc9
Brad Abrahams <email@bradabrahams.co.uk>
2017-10-20 13:19:40 +0100

    Don't display empty usertext element if not needed

--- assets/js/components/FullPost.jsx
+++ assets/js/components/FullPost.jsx
@@ -17,12 +17,16 @@ class FullPost extends React.Component {
       <a href={this.props.post.url} target="_blank" rel="noopener noreferrer">{this.props.post.title}</a>
     );
 
+    const userText = this.props.post.selftext ? (
+      <div className="full-post__header__text usertext" dangerouslySetInnerHTML={{ __html: this.props.post.selftext_html }} />
+    ) : null;
+
     return (
       <div id="full-post" className="full-post">
         <header className="full-post__header card">
           <h2 className="full-post__header__title">{title}</h2>
           <PostDetails post={this.props.post} />
-          <div className="full-post__header__text usertext" dangerouslySetInnerHTML={{ __html: this.props.post.selftext_html }} />
+          {userText}
           <PostMedia post={this.props.post} />
         </header>
         <Comments comments={this.props.post.comments} op={this.props.post.author.name} />
