287096de743b53f6c15da4f132b1e10ab7b1e176
Brad Abrahams <email@bradabrahams.co.uk>
2017-10-20 16:51:43 +0100

    Make fetch more comments sort-of work
    When returning the the new state it doesn't contain the extra attributes like
    _more. This is needed because otherwise all of the 'load more comments' links
    disappear.

--- assets/js/actions/postActions.js
+++ assets/js/actions/postActions.js
@@ -17,3 +17,15 @@ export const fetchPost = postId => (dispatch) => {
                  dispatch(fetchPostSuccess(post));
                });
 };
+
+export const fetchMoreCommentsSuccess = (commentId, fetchedComments) => ({
+  type: 'FETCH_MORE_COMMENTS_SUCCESS',
+  commentId,
+  fetchedComments,
+});
+
+export const fetchMoreComments = comment => (dispatch) => {
+  comment.replies.fetch_more(comment.replies._more.count)
+         .then(fetchedComments =>
+           dispatch(fetchMoreCommentsSuccess(comment.id, fetchedComments)));
+};
--- assets/js/components/Comment.jsx
+++ assets/js/components/Comment.jsx
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 import { plural } from '../utils';
+import { fetchMoreComments } from '../actions/postActions';
 
 class Comment extends React.Component {
   render() {
@@ -14,14 +16,17 @@ class Comment extends React.Component {
       authorCssClass += ' is-op';
     }
 
-    const moreComments = (comment.replies._more && comment.replies._more.children.length) ? (
-      <div className="comment__more-comments">
+    const moreComments = (comment.replies && comment.replies._more && comment.replies._more.children.length) ? (
+      <div
+        className="comment__more-comments"
+        onClick={() => this.props.fetchMoreComments(comment)}
+      >
         load more comments
         <span> ({comment.replies._more.count} replies)</span>
       </div>
     ) : null;
 
-    const continueThread = (!comment.replies.length && comment.replies._query.comment) ? (
+    const continueThread = (comment.replies && !comment.replies.length && comment.replies._query.comment) ? (
       <a
         className="comment__continue-thread"
         href={'https://www.reddit.com/' + this.props.permalink + comment.replies._query.comment}
@@ -43,13 +48,15 @@ class Comment extends React.Component {
           <span className="comment__header__votes">{score + ' ' + plural(score, ['vote', 'votes'])}</span>
         </header>
         <div className="usertext" dangerouslySetInnerHTML={{ __html: this.props.comment.body_html }} />
-        {comment.replies.map(comm =>
+        {comment.replies && comment.replies.map(comm => (
           <Comment
             key={comm.id}
             comment={comm}
             op={this.props.op}
             permalink={this.props.permalink}
-          />)}
+            fetchMoreComments={this.props.fetchMoreComments}
+          />
+        ))}
         {moreComments}
         {continueThread}
       </div>
@@ -61,6 +68,15 @@ Comment.propTypes = {
   comment: PropTypes.object,
   op: PropTypes.string,
   permalink: PropTypes.string,
+  fetchMoreComments: PropTypes.func,
 };
 
-export default Comment;
+const mapStateToProps = () => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchMoreComments: comment =>
+    dispatch(fetchMoreComments(comment)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Comment);
