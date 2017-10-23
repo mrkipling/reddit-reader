92a7cbc8cf47aa4030b910436f2d05af78595772
Brad Abrahams <email@bradabrahams.co.uk>
2017-10-23 17:07:57 +0100

    Make up/downvoting happen immediately in the UI

--- assets/js/actions/votesActions.js
+++ assets/js/actions/votesActions.js
@@ -7,15 +7,15 @@ export const voteCommentSuccess = (commentId, direction) => ({
 export const voteComment = (comment, commentLikes, up) => (dispatch) => {
   // unvote
   if ((up && commentLikes) || (!up && commentLikes !== null && commentLikes === false)) {
-    return comment.unvote().then(() =>
-      dispatch(voteCommentSuccess(comment.id, 'unvote')));
+    comment.unvote();
+    return dispatch(voteCommentSuccess(comment.id, 'unvote'));
   }
 
   if (up) {
-    return comment.upvote().then(() =>
-      dispatch(voteCommentSuccess(comment.id, 'upvote')));
+    comment.upvote();
+    return dispatch(voteCommentSuccess(comment.id, 'upvote'));
   }
 
-  return comment.downvote().then(() =>
-    dispatch(voteCommentSuccess(comment.id, 'downvote')));
+  comment.downvote();
+  return dispatch(voteCommentSuccess(comment.id, 'downvote'));
 };
