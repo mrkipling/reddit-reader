59e8214e5201f62715f4b261661a2579c1ed06da
Brad Abrahams <email@bradabrahams.co.uk>
2017-10-23 17:05:52 +0100

    voteActions => votesActions

--- assets/js/components/Comment.jsx
+++ assets/js/components/Comment.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 import { plural, timeAgo, formatNumber } from '../utils';
 import { fetchMoreComments } from '../actions/moreCommentsActions';
-import { voteComment } from '../actions/voteActions';
+import { voteComment } from '../actions/votesActions';
 
 class Comment extends React.Component {
   render() {
